8e7f85eda124d71275525c7b51d50b236b3523ea
dinhnhat0401 <nhatphutho@gmail.com>
2020-06-27 19:20:20 +0900

    Completed the react client side codes

--- client/src/App.js
+++ client/src/App.js
@@ -1,43 +1,26 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import ApolloClient from 'apollo-boost';
+import { ApolloProvider } from 'react-apollo';
+
+// components
+import BookList from './components/BookList';
+import AddBook  from './components/AddBook';
+
+// apollo client setup
+const client = new ApolloClient({
+  uri: 'http://localhost:9000/graphql'
+});
 
 class App extends Component {
-
-  constructor(props) {
-    super(props);
-    this.state = { apiResponse: "" };
-  }
-
-  callAPI() {
-    fetch("http://localhost:9000/testAPI")
-      .then(res => res.text())
-      .then(res => this.setState({ apiResponse: res }));
-  }
-
-  componentWillMount() {
-    this.callAPI();
-  }
-
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <p className="App-intro">{ this.state.apiResponse }</p>
-        </header>
-      </div>
+      <ApolloProvider client = { client }>
+        <div id = "main">
+          <h1>Reading List</h1>
+          <BookList />
+          <AddBook />
+        </div>
+      </ApolloProvider>
     );
   }
 }
